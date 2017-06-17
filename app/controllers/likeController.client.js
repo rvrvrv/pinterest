8431a9a340f5711ec838c7211ab5b8fefd000c72
Rich <ibanez19@sbcglobal.net>
2017-06-17 14:40:13 +0000

    Building out unlike functionality

--- app/controllers/likeController.client.js
+++ app/controllers/likeController.client.js
@@ -5,26 +5,38 @@
 
 //Like (or unlike) a pin
 function likePin(link, unlike) {
+
     //Store pin information for API call
     let likeReq = {
         url: encodeURIComponent($(link).data('url')),
         owner: $(link).data('owner')
     };
-    console.log(link);
+
     //Make the appropriate API call
     let method = unlike ? 'DELETE' : 'PUT';
-    ajaxFunctions.ajaxRequest(method, `/api/like/${JSON.stringify(likeReq)}`, (res) => {
-        //Handle errors
-        if (res === 'error') return errorMsg('An error occurred while trying to like the pin.');
-        if (res === 'exists') return errorMsg('You already like this pin!');
-        let likeCount = $(link).next();
-        console.log(likeCount);
-        //If like is successful, notify the user and update UI
-        Materialize.toast('Liked it!', 2000);
-        likeCount.html(`${+likeCount.text() + 1}`);
-        likeBtnSwitch(link, true);
-    });
 
+    ajaxFunctions.ajaxRequest(method, `/api/like/${JSON.stringify(likeReq)}`, (res) => {
+        if (res === 'error') return errorMsg('An error has occured.');
+
+        let likeCount = $(link).next();
+
+        //After trying to unlike pin
+        if (unlike) {
+            if (res === 'no') return errorMsg('You don\'t like this pin yet!');
+            //If unlike is successful, notify the user and update UI
+            Materialize.toast('Unliked it!', 2000);
+            likeCount.html(`${+likeCount.text() - 1}`);
+            likeBtnSwitch(link);
+        }
+        //After trying to like pin
+        else {
+            if (res === 'exists') return errorMsg('You already like this pin!');
+            //If like is successful, notify the user and update UI
+            Materialize.toast('Liked it!', 2000);
+            likeCount.html(`${+likeCount.text() + 1}`);
+            likeBtnSwitch(link, true);
+        }
+    });
 }
 
 //Switch like button (to like or unlike)
